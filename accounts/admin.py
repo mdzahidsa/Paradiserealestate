3a5ae40da5bd71d3a43b99b759639cef3dcee93a
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2023-08-04 00:43:01 +0100

    custom user model  config

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email','username','first_name','last_name','role','is_active','last_login')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
