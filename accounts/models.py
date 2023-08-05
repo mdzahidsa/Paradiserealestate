76a77434f21e4bf5d1ecdda9330a4e4f4fc677fa
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2023-08-05 01:05:05 +0100

    Userprofile and signls

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField,ForeignKey
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -72,4 +75,25 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+class UserProfile(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photo', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=8, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+
 
---
+++ accounts/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import User,UserProfile
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    print(created)
+    if created:
+        UserProfile.objects.create(user=instance)
+        print("user profile is created")
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            #creating user profile if doesnt exist.
+            UserProfile.objects.create(user=instance)
+            print("user is updated")
+
+
+#post_save.connect(post_save_create_profile_reciever, sender=User)
