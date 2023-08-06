7a6df84a124423023dc1f8786fba55871b50f224
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2023-08-06 04:10:58 +0100

    Tenant register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import UserForm
+from .models import User
 # Create your views here.
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.TENANT
+            form.save()
+            return redirect('registerUser')
+        else:
+            print('Invalid form')
+    else: 
+        form = UserForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
