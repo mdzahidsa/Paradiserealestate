04ecd6051a936b0b142a0a06b54a6f4f900809ed
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2023-08-17 15:51:38 +0100

    Google Api & profile address

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,8 +1,11 @@
 from owner.models import Owner
-
+from django.conf import settings
 def get_owner(request):
     try:
         owner = Owner.objects.get(user=request.user)
     except:
         owner = None
     return dict(owner=owner)
+
+def get_google_api(request):
+    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
