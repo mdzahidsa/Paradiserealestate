79e764bceb2a4f7dc4c8d40543e16b3c20f29d04
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2023-09-11 19:11:23 +0100

    Uncomnt signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,20 +2,20 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User,UserProfile
 
-#@receiver(post_save, sender=User)
-#def post_save_create_profile_receiver(sender, instance, created, **kwargs):
- #   print(created)
-  #  if created:
-   #     UserProfile.objects.create(user=instance)
-    #    print("user profile is created")
-   # else:
-    #    try:
-     #       profile = UserProfile.objects.get(user=instance)
-      #      profile.save()
-       # except:
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    print(created)
+    if created:
+        UserProfile.objects.create(user=instance)
+        print("user profile is created")
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
             #creating user profile if doesnt exist.
-          #  UserProfile.objects.create(user=instance)
-        ##    print("user is updated")
+            UserProfile.objects.create(user=instance)
+            print("user is updated")
 
 
 #post_save.connect(post_save_create_profile_reciever, sender=User)
